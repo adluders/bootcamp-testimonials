0c28b9da8a7df5c93950802de0b13895ad7298f5
Adler Luders <adlerluders@gmail.com>
2020-05-15 17:48:37 -0400

    Built UI

---
+++ app.js
@@ -0,0 +1,9 @@
+const prevBtn = document.getElementById("prev"),
+  nextBtn = document.getElementById("next"),
+  testimonials = document.querySelectorAll(".testimonial");
+
+let currentItem = 0;
+
+// Have an array of the testimonials
+// Create a function to change current item to currItem + 1
+// Create a function to change current item to currItem - 1
