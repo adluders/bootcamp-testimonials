94737233b9f5d0e898c76686a2f5acff50ebb928
Adler Luders <adlerluders@gmail.com>
2020-05-16 18:21:24 -0400

    Added toggle functionality

--- app.js
+++ app.js
@@ -5,5 +5,24 @@ const prevBtn = document.getElementById("prev"),
 let currentItem = 0;
 
 // Have an array of the testimonials
-// Create a function to change current item to currItem + 1
-// Create a function to change current item to currItem - 1
+const testimonialArr = [...testimonials];
+
+function hideAllSections() {
+  testimonialArr.forEach((t) => (t.style.display = "none"));
+}
+
+function showNextSection() {
+  hideAllSections();
+  currentItem > 0 ? (currentItem = 0) : currentItem++;
+  testimonialArr[currentItem].style.display = "grid";
+}
+
+function showPrevSection() {
+  currentItem--;
+  hideAllSections();
+  currentItem < 0 && (currentItem = 1);
+  testimonialArr[currentItem].style.display = "grid";
+}
+
+nextBtn.addEventListener("click", showNextSection);
+prevBtn.addEventListener("click", showPrevSection);
